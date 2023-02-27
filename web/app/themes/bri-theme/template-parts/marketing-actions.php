bbd112756e5989f494b819a538b996f5f5de3e0b
Дмитрий Резвых <rezvykh@mail.ru>
2023-02-27 23:36:53 +0400

    added marketig actions block

---
+++ web/app/themes/bri-theme/template-parts/marketing-actions.php
@@ -0,0 +1,66 @@
+<?php 
+    $start_date = strtotime(post_custom('from_date'));
+    $finish_date = strtotime(post_custom('to_date'));
+    $intlFormatter = new IntlDateFormatter('ru_RU', IntlDateFormatter::SHORT, IntlDateFormatter::SHORT);
+    $intlFormatter->setPattern('d MMMM');
+
+    /**
+	 * How much time left
+	 *
+	 * @param mixed $date
+	 * @return
+	 */
+	function downcounter($date){
+	    $check_time = $date - time();
+	    if($check_time <= 0){
+	        return false;
+	    }
+
+	    $days = floor($check_time/86400);
+	    $hours = floor(($check_time%86400)/3600);
+	    $minutes = floor(($check_time%3600)/60);
+
+	    $str = '';
+	    if($days > 0) $str .= $days.'д'.':';
+	    if($hours > 0) $str .= $hours.'ч'.':';
+	    if($minutes > 0) $str .= $minutes.'м'.':';
+
+	    return $str;
+	}
+?>
+
+<li class="discount__slide swiper-slide">
+    <article class="discount-card">
+    <div class="discount-card__left">
+        <div class="discount-card__top">
+        <span class="discount-card__badge badge badge--yellow"> Акции </span>
+        <span class="discount-card__date">С <?php echo $intlFormatter->format($start_date); ?> до <?php echo $intlFormatter->format($finish_date); ?></span>
+        </div>
+        <div class="discount-card__middle">
+        <h3 class="discount-card__title">
+            <a href="<?php echo esc_url( get_permalink() );?>">
+            <?php the_title(); ?>
+            </a>
+        </h3>
+        <p class="discount-card__text">
+            <?php echo get_the_excerpt(); ?>
+        </p>
+        </div>
+        <div class="discount-card__time">
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+            d="M7.5 13.125C10.6066 13.125 13.125 10.6066 13.125 7.5C13.125 4.3934 10.6066 1.875 7.5 1.875C4.3934 1.875 1.875 4.3934 1.875 7.5C1.875 10.6066 4.3934 13.125 7.5 13.125Z"
+            stroke="#222222" stroke-miterlimit="10" />
+            <path d="M7.5 7.5H10.7812" stroke="#222222" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M9.82031 9.82031L7.5 7.5" stroke="#222222" stroke-width="1.5" stroke-linecap="round"
+            stroke-linejoin="round" />
+        </svg>
+
+        <span><?php echo downcounter($finish_date); ?></span>
+        </div>
+    </div>
+    <a class="discount-card__right" href="#">
+        <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="discount" />
+    </a>
+    </article>
+</li>
--- web/app/themes/bri-theme/template-parts/news-preview.php
+++ web/app/themes/bri-theme/template-parts/news-preview.php
@@ -4,11 +4,11 @@
         <span class="news-card__date"><?php echo get_the_date('j F Y'); ?></span>
         <div class="news-card__bottom">
         <h3 class="news-card__title">
-            <a href="<?php echo esc_url( get_permalink() )?>">
+            <a href="<?php echo esc_url( get_permalink() );?>">
                 <?php the_title(); ?>
             </a>
         </h3>
-        <a class="news-card__link" href="<?php echo esc_url( get_permalink() )?>">Подробнее</a>
+        <a class="news-card__link" href="<?php echo esc_url( get_permalink() );?>">Подробнее</a>
         </div>
     </div>
     </article>
